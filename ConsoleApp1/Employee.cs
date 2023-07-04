b1bfcdce5eeb59968c560a7ee7e5c70a1b4e9728
Egar.net <Egar.net2000@gmail.com>
2023-07-04 18:32:08 +0600

    Add ConsoleApp1

---
+++ ConsoleApp1/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int BirthAge { get; set; }
+        public int Pay { get; set; }
+
+        public Employee(string firstName, string lastName, int birthAge, int pay)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthAge = birthAge;
+            Pay = pay;
+        }
+    }
+
+    internal class EmployeeComparer : IComparer<Employee>
+    {
+        public int Compare(Employee x, Employee y)
+        {
+            return string.Compare(x.FirstName, y.FirstName);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1;
+using System;
+using System.Collections.Generic;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        List<Employee> employees = new List<Employee>()
+        {
+            new Employee("John", "Duo", 25, 50000),
+            new Employee("Alice De", "Smithen", 30, 60000),
+            new Employee("John", "Wick", 43, 55000)
+        };
+
+        employees.Sort(new EmployeeComparer());
+
+        foreach (var employee in employees)
+        {
+            Console.WriteLine($"{employee.FirstName} {employee.LastName}, год рождения: {employee.BirthAge}, зарплата: {employee.Pay}");
+        }
+    }
+}
